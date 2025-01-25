e98233a38cff7e45e1d6bdb8f2129186c964cf98
Ezra Ju <96998749+EzraJJu@users.noreply.github.com>
2025-01-25 16:07:49 -0500

    extra tidying

--- src/main/java/frc/robot/commands/Autonomous/Autos.java
+++ src/main/java/frc/robot/commands/Autonomous/Autos.java
@@ -1,8 +1,8 @@
+package frc.robot.commands.autonomous;
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Autonomous;
 
 
 import edu.wpi.first.wpilibj2.command.Command;
