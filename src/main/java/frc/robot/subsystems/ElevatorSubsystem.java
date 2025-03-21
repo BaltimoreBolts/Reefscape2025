d1342e378756a03584f55b1f1123ccb6a08b4755
Ezra Ju <96998749+EzraJJu@users.noreply.github.com>
2025-03-21 14:47:44 -0400

    unbroken code?

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -34,7 +34,7 @@ public class ElevatorSubsystem extends SubsystemBase {
 
         motor1Config.inverted(false).idleMode(IdleMode.kBrake);
         motor2Config.inverted(false).follow(ElevatorConstants.kElevatorLeft).idleMode(IdleMode.kBrake);
-        motor1Config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(10.0, 0.0, 0.0);
+        motor1Config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(0.025, 0, 0.00001);
 
         m_motor1.configure(
                 motor1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
