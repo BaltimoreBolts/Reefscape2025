511af4f5a5da65ca5846f9943ddbf23b8608bad5
Ezra Ju <96998749+EzraJJu@users.noreply.github.com>
2025-10-16 19:37:23 -0400

    commented out old shooter for testing

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -14,38 +14,38 @@ import frc.robot.Constants.ShooterConstants.ScoringTarget;
 import frc.robot.Constants.inputConstants.digitalInputConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
-    private final SparkMax m_motor1 =
-            new SparkMax(ShooterConstants.kShooterPort, MotorType.kBrushless);
+    // private final SparkMax m_motor1 =
+    //         new SparkMax(ShooterConstants.kShooterPort, MotorType.kBrushless);
 
-    private final DigitalInput m_firstSensor = new DigitalInput(digitalInputConstants.kFirstSensor);
+    // private final DigitalInput m_firstSensor = new DigitalInput(digitalInputConstants.kFirstSensor);
 
-    SparkBaseConfig motor1Config = new SparkMaxConfig();
+    // SparkBaseConfig motor1Config = new SparkMaxConfig();
 
     // Defaults
-    private final double speedModifier = 0.0;
-    private ScoringTarget scoringTarget = ScoringTarget.L1;
+    // private final double speedModifier = 0.0;
+    // private ScoringTarget scoringTarget = ScoringTarget.L1;
 
-    public ShooterSubsystem() {
+    // public ShooterSubsystem() {
 
-        motor1Config.inverted(true).idleMode(IdleMode.kCoast);
+    //     motor1Config.inverted(true).idleMode(IdleMode.kCoast);
 
-        m_motor1.configure(
-                motor1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    }
+    //     m_motor1.configure(
+    //             motor1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // }
 
     // Methods for sensors
-    public boolean getFirstDigitalInput() {
-        return m_firstSensor.get();
-    }
+    // public boolean getFirstDigitalInput() {
+    //     return m_firstSensor.get();
+    // }
 
     // Scoring target methods
     public void setScoringTarget(ScoringTarget target) {
-        scoringTarget = target;
+        // scoringTarget = target;
     }
 
-    public ScoringTarget getScoringTarget() {
-        return scoringTarget;
-    }
+    // public ScoringTarget getScoringTarget() {
+    //     // return scoringTarget;
+    // }
 
     // More shooter methods
     public void stopShooter() {
@@ -56,10 +56,10 @@ public class ShooterSubsystem extends SubsystemBase {
     public void periodic() {}
 
     public void setSpeed(double speed) {
-        double speedToSet = speed + speedModifier;
-        if (speed == 0.0) {
-            speedToSet = 0.0;
-        }
-        m_motor1.set(speedToSet);
+        // double speedToSet = speed + speedModifier;
+        // if (speed == 0.0) {
+        //     speedToSet = 0.0;
+        // }
+        // m_motor1.set(speedToSet);
     }
 }
